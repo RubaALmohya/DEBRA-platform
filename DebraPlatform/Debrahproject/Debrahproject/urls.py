e7e01b7e2e93f84ffcbee692aee0cd15c76aa654
RubaALmohya <ruba12399@gmail.com>
2022-06-12 23:55:08 +0300

    adding users and experts views

--- DebraPlatform/Debrahproject/Debrahproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path , include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('moderators/',include('Moderators.urls')),
-    #path('experts/', include('Experts.urls')),
-    #path('users/', include('Users.urls'))
-]
